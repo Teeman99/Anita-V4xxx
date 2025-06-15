ad8bffa30d1b3cd4e39964f039ea735d49cd2f3e
Teeman99 <ngwenyathabani93@gmail.com>
2025-06-15 12:44:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.anitav4 = process.env.BOT_NAME || "Patience";
+global.ownername = process.env.OWNER_NAME || "YounGody";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
+global.author = process.env.AUTHOR || "YounGody";
+global.channelname = process.env.CHANNEL_NAME || "";
+global.channeljid = process.env.CHANNEL_JID || "";
 
 // Auto-Features
 global.autoTyping = toBool(process.env.AUTO_TYPING);
